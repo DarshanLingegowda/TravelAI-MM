b94927fbdf72a7161ab60881772b48e1179b236a
Darshan <darshanl1711@gmail.com>
2025-12-27 00:08:47 +0530

    Update vector_store.py

--- retrieval/vector_store.py
+++ retrieval/vector_store.py
@@ -1,1 +1,12 @@
+class VectorStore:
+    def __init__(self):
+        self.vectors = []
+        self.metadata = []
+
+    def add(self, vector, meta):
+        self.vectors.append(vector)
+        self.metadata.append(meta)
+
+    def search(self, query_vector):
+        return []
 
